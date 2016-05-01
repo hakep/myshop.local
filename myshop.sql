a0a8538da601895eb995b0093579e2851e0ad572
OZ <first@xakep.ru>
2016-05-01 05:06:45 +0300

    purchase-1

---
+++ myshop.sql
@@ -0,0 +1,143 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10.10
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Май 01 2016 г., 05:04
+-- Версия сервера: 5.5.45
+-- Версия PHP: 5.5.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `myshop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `categories`
+--
+
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+
+--
+-- Дамп данных таблицы `categories`
+--
+
+INSERT INTO `categories` (`id`, `parent_id`, `name`) VALUES
+(1, 0, 'Телефоны'),
+(2, 0, 'Планшеты'),
+(3, 1, 'Телефоны Samsung'),
+(4, 1, 'Телефоны Apple'),
+(5, 2, 'Планшеты Apple'),
+(6, 2, 'Планшеты Acer'),
+(7, 2, 'Планшеты Samsung');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `orders`
+--
+
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `date_created` datetime NOT NULL,
+  `date_payment` datetime DEFAULT NULL,
+  `date_modification` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `status` tinyint(4) NOT NULL DEFAULT '0',
+  `comment` text NOT NULL,
+  `user_ip` varchar(15) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `products`
+--
+
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` text NOT NULL,
+  `price` float NOT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
+
+--
+-- Дамп данных таблицы `products`
+--
+
+INSERT INTO `products` (`id`, `category_id`, `name`, `description`, `price`, `image`, `status`) VALUES
+(1, 3, 'Samsung Gem', 'The Samsung Gem\\u2122 brings you everything that you would expect and more from a touch display smart phone \\u2013 more apps, more features and a more affordable price.', 6001, '1.jpg', 1),
+(2, 3, 'Samsung Galaxy Tab', 'The Samsung Showcaseu2122 delivers a cinema quality experience like youu2019ve never seen before. Its innovative 4u201d touch display technology provides rich picture brilliance, even outdoors', 4501, '2.jpg', 1),
+(3, 3, 'Samsung Showcase', 'An experience to cheer about.', 6561, '3.jpg', 1),
+(4, 3, 'MOTOROLA BRAVO', 'Are you ready for everything life throws your way?', 13401, '4.jpg', 0),
+(5, 3, 'T-Mobile myTouch 4G', 'The T-Mobile myTouch 4G is a premium smartphone designed to deliver blazing fast 4G speeds so that you can video chat from practically anywhere, with or without Wi-Fi.', 9801, '5.jpg', 1),
+(6, 3, 'Samsung Mesmerize', 'The Samsung Mesmerizeu2122 delivers a cinema quality experience like youu2019ve never seen before. Its innovative 4u201d touch display technology provides rich picture brilliance,even outdoors', 6044, '6.jpg', 1),
+(7, 3, 'SANYO ZIO', 'The Sanyo Zio by Kyocera is an Android smartphone with a combination of ultra-sleek styling, strong performance and unprecedented value.', 3301, '7.jpg', 1),
+(8, 3, 'Samsung Transform', 'The Samsung Transformu2122 brings you a fun way to customize your Android powered touch screen phone to just the way you like it through your favorite themed u201cSprint ID Service Packu201d.', 661, '8.jpg', 1),
+(9, 4, 'T-Mobile G2', 'The T-Mobile G2 with Google is the first smartphone built for 4G speeds on T-Mobile''s new network. Get the information you need, faster than you ever thought possible.', 4901, '9.jpg', 1),
+(10, 4, 'Motorola CHARM', 'Motorola CHARM fits easily in your pocket or palm.  Includes MOTOBLUR service.', 11101, '10.jpg', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `purchase`
+--
+
+CREATE TABLE IF NOT EXISTS `purchase` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `price` float NOT NULL,
+  `amount` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) NOT NULL,
+  `pwd` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `adress` text NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `pwd`, `name`, `phone`, `adress`) VALUES
+(1, 'oz', '202cb962ac59075b964b07152d234b70', 'Сергей', '123456789', 'ковдор'),
+(5, 'first@xakep.ru', 'c81e728d9d4c2f636f067f89cc14862c', 'oz2', '8921-123-45-561', '');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
